199241f38da33432456039bb2a28cc4a1b16257f
nadlev <nadiya.levytska.jr@gmail.com>
2023-05-18 18:31:39 +0300

    Initial Commit

---
+++ CircleTabBarAnimation/CircleTabBarAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleTabBarAnimationApp.swift
+//  CircleTabBarAnimation
+//
+//  Created by Надежда Левицкая on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleTabBarAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
